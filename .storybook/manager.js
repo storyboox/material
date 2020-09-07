e2276d6d780f1818064645316efbea742763bbac
Ziv.Perry <ziv.perry@gmail.com>
2020-09-07 14:39:53 +0300

    theme

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import storyboox from './storyboox.theme';
+
+addons.setConfig({
+  theme: storyboox,
+});
---
+++ .storybook/storyboox.theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Material Storyboox',
+  brandUrl: 'https://storyboox.github.io/material/',
+  brandImage: 'https://storyboox.github.io/assets/logo-storyboox-120.png',
+});
